23e9b561aa56eee81653a5ba0416d0363263c7d5
houndhollis <houndhollis9797@gmail.com>
2026-01-17 22:42:20 +0900

    chore: 로그인 페이지 enter 가능하도록 변경

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -10,23 +10,30 @@ export const LoginForm = () => {
 
   const { login, isLoading } = useLogin();
 
-  const handleSubmit = useCallback(() => {
-    if (!validateEmail(email)) {
-      setError('유효한 이메일을 입력해주세요.');
-      return;
-    }
+  const handleSubmit = useCallback(
+    (e?: React.FormEvent) => {
+      e?.preventDefault();
 
-    setError('');
-    login(email);
-  }, [email, login]);
+      if (!validateEmail(email)) {
+        setError('유효한 이메일을 입력해주세요.');
+        return;
+      }
+
+      setError('');
+      login(email);
+    },
+    [email, login],
+  );
 
   return (
     <div className="min-h-full flex items-center justify-center">
       <div className="max-w-140 px-4 w-full flex flex-col items-center justify-between">
         <h1 className="text-[42px] text-[#374553] font-bold">반갑습니다.</h1>
         <p className="text-[26px] text-[#646F7C]">로그인 정보를 입력해주세요.</p>
-
-        <div className=" w-full py-4 px-6 mt-10 bg-[#F7F8F9] border border-[#D0D6DB] rounded-[20px]">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full py-4 px-6 mt-10 bg-[#F7F8F9] border border-[#D0D6DB] rounded-[20px] flex flex-col"
+        >
           <label className="block text-[14px] text-[#646F7C] mb-2">
             이메일 주소를 입력해주세요.
           </label>
@@ -38,16 +45,16 @@ export const LoginForm = () => {
             className="w-full mb-8 p-6 border border-[#374553] rounded-xl text-[14px] focus:outline-none focus:none"
           />
           <Button
+            type="submit"
             disabled={isLoading}
             color="black"
             active
-            onClick={handleSubmit}
             className="w-full py-6 text-[16px]"
           >
             로그인 하기
           </Button>
           {error && <p className="mt-1 text-red-500">{error}</p>}
-        </div>
+        </form>
       </div>
     </div>
   );
