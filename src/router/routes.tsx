b872d26997c223ecc90b6c1779a6eb3f3a99f302
houndhollis <houndhollis9797@gmail.com>
2026-01-18 03:18:27 +0900

    chore: 파일명 공백제거

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,6 @@
 import { AuthGuard } from '@/features/auth/components/AuthGuard';
 import { AuthPage } from '@/features/auth/page';
-import { OrdersPage } from '@/features/exchange/\borders/page';
+import { OrdersPage } from '@/features/exchange/orders/page';
 import { ExchangeLayout } from '@/features/exchange/components/ExchangeLayout';
 import { ExchangePage } from '@/features/exchange/page';
 import { AppLayout } from '@/shared/components/AppLayout';
