e0e9c5064c21cb7e806e324d59ad7c5ab6e05702
houndhollis <houndhollis9797@gmail.com>
2026-01-17 19:49:10 +0900

    feat: eslint, prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,41 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
+    rules: {
+      curly: 'error',
+      'prefer-const': 'error',
+      'no-const-assign': 'warn',
+      'no-var': 'error',
+      'no-nested-ternary': 'error',
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
     languageOptions: {
+      parser: tsParser,
       ecmaVersion: 2020,
+      sourceType: 'module',
       globals: globals.browser,
     },
   },
-])
+]);
